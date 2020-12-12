ec11f505789d5271b3822f9d79405dbfb29ba9f8
jep953 <jep953@student.bham.ac.uk>
2020-12-12 19:51:03 +0000

    add day10

--- solutions/day10/day10.py
+++ solutions/day10/day10.py
@@ -1,2 +1,24 @@
 with open('input.txt') as input:
     lines = input.read().split('\n')
+    lines = [ int(line) for line in lines ]
+lines += [max(lines) + 3]
+lines += [0]
+lines.sort()
+
+
+# PART 1
+
+def multiply_counts(lines):
+    diffs = [lines[n] - lines[n-1] for n in range(1, len(lines))]
+    return diffs.count(1) * diffs.count(3)
+
+
+# PART 2
+
+def count_arrangements(lines):
+    diffs = [lines[n] - lines[n-1] for n in range(1, len(lines))]
+    
+
+    return ''
+
+print(multiply_counts(lines))
